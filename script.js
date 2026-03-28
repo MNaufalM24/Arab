fde0e423a6f4a5163adad14633e984626f0f8dbc
Muhammad Naufal Mahadhitia <naufalmm2003@upi.edu>
2026-03-28 17:43:19 +0700

    Add labels to subjects and verbs in script.js

--- script.js
+++ script.js
@@ -3,15 +3,39 @@ let history = [];
 
 // DATA
 const subjects = [
-  { key: "ana", ar: "أنا", en: "I" },
-  { key: "anta", ar: "أنتَ", en: "You (m)" },
-  { key: "huwa", ar: "هو", en: "He" }
+  { key: "ana", ar: "أنا", en: "I", label: "sg" },
+  { key: "nahnu", ar: "نحن", en: "We", label: "pl" },
+
+  { key: "anta", ar: "أنتَ", en: "You", label: "m" },
+  { key: "anti", ar: "أنتِ", en: "You", label: "f" },
+  { key: "antuma", ar: "أنتما", en: "You two", label: "dual" },
+  { key: "antum", ar: "أنتم", en: "You", label: "pl m" },
+  { key: "antunna", ar: "أنتنّ", en: "You", label: "pl f" },
+
+  { key: "huwa", ar: "هو", en: "He", label: "m" },
+  { key: "hiya", ar: "هي", en: "She", label: "f" },
+  { key: "huma_m", ar: "هما", en: "They two", label: "dual m" },
+  { key: "huma_f", ar: "هما", en: "They two", label: "dual f" },
+  { key: "hum", ar: "هم", en: "They", label: "pl m" },
+  { key: "hunna", ar: "هنّ", en: "They", label: "pl f" }
 ];
 
 const verbs = {
   ana: "أتكلم",
+  nahnu: "نتكلم",
+
   anta: "تتكلم",
-  huwa: "يتكلم"
+  anti: "تتكلمين",
+  antuma: "تتكلمان",
+  antum: "تتكلمون",
+  antunna: "تتكلمْنَ",
+
+  huwa: "يتكلم",
+  hiya: "تتكلم",
+  huma_m: "يتكلمان",
+  huma_f: "تتكلمان",
+  hum: "يتكلمون",
+  hunna: "يتكلمن"
 };
 
 const places = [
@@ -38,7 +62,7 @@ function generate() {
       subject.ar + " " + verb + (place ? " " + place.ar : "");
 
     const sentenceEn =
-      subject.en + " speak " + (place ? place.en : "");
+      subject.en + " speak" + (place ? " " + place.en : "");
 
     newSentence = {
       ar: sentenceAr,
@@ -61,7 +85,8 @@ function generate() {
 
   current = newSentence.ar;
 
-  document.getElementById("question").innerText = newSentence.en;
+  document.getElementById("question").innerText =
+  newSentence.en + " (" + subject.label + ")";
   document.getElementById("answer").style.display = "none";
 }
 
